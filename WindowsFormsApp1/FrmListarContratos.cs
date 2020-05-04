f3e01b4cc1c8fda5fb2ff73575d3fb496bdf4169
Felipe Ignacio Herrera Soto <felipe_2222@hotmail.com>
2020-05-04 03:15:22 -0400

    123123

--- WindowsFormsApp1/FrmListarContratos.cs
+++ WindowsFormsApp1/FrmListarContratos.cs
@@ -12,9 +12,50 @@ namespace WindowsFormsApp1
 {
     public partial class FrmListarContratos : Form
     {
+        private DataTable dt;
+        private List<Contrato> contratos;
+        private DataView dv;
+        public Contrato rContrato;
         public FrmListarContratos()
         {
             InitializeComponent();
+            contratos = TContrato.ListarContrato();
+            dt = new DataTable();            
+            dt.Columns.Add("Rut");
+            dt.Columns.Add("Nombre");
+            dt.Columns.Add("Actividad");
+            dt.Columns.Add("Tipo");
+            dt.Columns.Add("Razon Social");    
+            dt.Columns.Add("Mail");
+            dt.Columns.Add("Direccion");
+            dt.Columns.Add("Telefono");
+            dv = new DataView(dt);
+            fillDataTable(contratos);
+            dtg_Contratos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dtg_Contratos.DataSource = dv;
+           
+            
+
+
+        }        
+        private void fillDataTable(List<Contrato> contratos)
+        {
+            foreach (var contrato in contratos)
+            {
+                dt.Rows.Add(
+                    contrato.numeroContrato,
+                    contrato.rutCli,
+                    contrato.idTipo,
+                    contrato.creacion,
+                    contrato.termino,
+                    contrato.fechaHoraInicio,
+                    contrato.fechaHoraTermino,
+                    contrato.direccionCon,
+                    contrato.estaVigente,                   
+                    contrato.observaciones
+                    );
+            }
+        
         }
 
         private void btn_cancelarLc_Click(object sender, EventArgs e)
